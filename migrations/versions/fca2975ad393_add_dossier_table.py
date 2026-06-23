eb89b184fc5e322ed9af448c2ff2595c6218ee0e
Philocles anais <anais_philocles@yahoo.fr>
2026-06-23 17:19:01 +0200

    fix dossier migration order

--- migrations/versions/fca2975ad393_add_dossier_table.py
+++ migrations/versions/fca2975ad393_add_dossier_table.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'fca2975ad393'
-down_revision = None
+down_revision = "5013bad4fa8d"
 branch_labels = None
 depends_on = None
 
